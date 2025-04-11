6e83f694694eab45a3dfd8b6c54af0118892f867
Alexander Denton <100879026+Atden04@users.noreply.github.com>
2025-04-11 11:39:15 +0100

    made simple

--- main.ts
+++ main.ts
@@ -1,40 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    if (true) {
+    if (biasValue > 0) {
         biasValue += -0.5
     } else {
         biasValue = 0
     }
 })
 input.onButtonPressed(Button.AB, function () {
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, speed)
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, speed)
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, speed)
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
+    basic.showNumber(biasValue)
 })
 input.onButtonPressed(Button.B, function () {
-    if (true) {
+    if (biasValue < 10) {
         biasValue += 0.5
     } else {
         biasValue = 10
     }
 })
-let speed = 0
-let distanceToWall = 0
 let biasValue = 0
 biasValue = 0
-distanceToWall = 2
-speed = 20
+let distanceToWall = 3
+let speed = 20
+let traverseMaze = false
+Kitronik_Move_Motor.turnRadius(Kitronik_Move_Motor.TurnRadii.Tight)
 let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
 moveMotorZIP.setZipLedColor(1, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
@@ -45,4 +31,23 @@ basic.forever(function () {
     moveMotorZIP.rotate(1)
     moveMotorZIP.show()
     basic.pause(100)
+})
+basic.forever(function () {
+    if (traverseMaze) {
+        while (Kitronik_Move_Motor.measure() > distanceToWall) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+        }
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorLeft, Kitronik_Move_Motor.MotorDirection.Forward, 50)
+        while (Kitronik_Move_Motor.measure() > distanceToWall) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+        }
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 50)
+        while (Kitronik_Move_Motor.measure() > distanceToWall) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+        }
+        Kitronik_Move_Motor.motorOn(Kitronik_Move_Motor.Motors.MotorRight, Kitronik_Move_Motor.MotorDirection.Forward, 50)
+        while (Kitronik_Move_Motor.measure() > distanceToWall) {
+            Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
+        }
+    }
 })
