ac40eb1ec9c0f14b9520932f53f416e622a26e7e
Alexander Denton <100879026+Atden04@users.noreply.github.com>
2025-04-28 15:49:09 +0100

    Simple measure distance.

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 def on_button_pressed_a():
-    Kitronik_Move_Motor.motor_balance(Kitronik_Move_Motor.SpinDirections.RIGHT, 1)
-    Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, 20)
+    if Kitronik_Move_Motor.measure() > distanceToWall:
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.FORWARD, speed)
+    else:
+        Kitronik_Move_Motor.stop()
+    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT, speed)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    Kitronik_Move_Motor.stop()
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
+speed = 0
+distanceToWall = 0
+biasValue = 0
+distanceToWall = 2
+speed = 20
 moveMotorZIP = Kitronik_Move_Motor.create_move_motor_zipled(4)
 moveMotorZIP.set_zip_led_color(0,
     Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.RED))
@@ -16,29 +20,10 @@ moveMotorZIP.set_zip_led_color(2,
     Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.GREEN))
 moveMotorZIP.set_zip_led_color(3,
     Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.YELLOW))
-Kitronik_Move_Motor.set_ultrasonic_units(Kitronik_Move_Motor.Units.INCHES)
+Kitronik_Move_Motor.set_ultrasonic_units(Kitronik_Move_Motor.Units.CENTIMETERS)
 
 def on_forever():
     moveMotorZIP.rotate(1)
     moveMotorZIP.show()
     basic.pause(100)
 basic.forever(on_forever)
-
-def on_forever2():
-    if Kitronik_Move_Motor.measure() <= 10:
-        basic.show_leds("""
-            . # . # .
-            . # . # .
-            . . . . .
-            # . . . #
-            . # # # .
-            """)
-    else:
-        basic.show_leds("""
-            . . . . .
-            . # . # .
-            . . . . .
-            . # # # .
-            # . . . #
-            """)
-basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,40 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    if (true) {
-        biasValue += -0.5
-    } else {
-        biasValue = 0
-    }
+	
 })
-input.onButtonPressed(Button.AB, function () {
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Left, speed)
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, speed)
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
-    Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.Right, speed)
-    while (Kitronik_Move_Motor.measure() > distanceToWall) {
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.Forward, speed)
-    }
-})
-input.onButtonPressed(Button.B, function () {
-    if (true) {
-        biasValue += 0.5
-    } else {
-        biasValue = 10
-    }
-})
-let speed = 0
-let distanceToWall = 0
 let biasValue = 0
-biasValue = 0
-distanceToWall = 2
-speed = 20
+let distanceToWall = 2
+let speed = 20
 let moveMotorZIP = Kitronik_Move_Motor.createMoveMotorZIPLED(4)
 moveMotorZIP.setZipLedColor(0, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Red))
 moveMotorZIP.setZipLedColor(1, Kitronik_Move_Motor.colors(Kitronik_Move_Motor.ZipLedColors.Blue))
@@ -45,4 +14,7 @@ basic.forever(function () {
     moveMotorZIP.rotate(1)
     moveMotorZIP.show()
     basic.pause(100)
+})
+basic.forever(function () {
+    basic.showNumber(Kitronik_Move_Motor.measure())
 })
